2db90a2c3534df57a6626f83e4297beac4436a2e
drefuz <109506760+iamdrefuz@users.noreply.github.com>
2023-06-07 23:05:07 +0500

    check

--- src/check.js
+++ src/check.js
@@ -3,4 +3,5 @@ export const userAdmin = [
       'I6Lou4XrrIQ77WFv98K85qTQUib2' ,//user
       'f6j1j7vH4tVBR3bfUSTRyDm2JC42',//user
       'Xag7LYfmySc0kBBflDXlUt1g8HK2',//support
+      'yEIkAK0EvkX4f0dlylwgjfMk7JH2'
     ]//     {
